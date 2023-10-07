d79057ef47661d7753b6224765fc36314a9d3177
ulfaini <ulfatulisnaini@gmail.com>
2023-10-07 10:23:18 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","root","","toko_online");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+?>
